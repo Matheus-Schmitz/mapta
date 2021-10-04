ac6681be108f365b0a34746c05e5a2b30415fb26
Matheus-Schmitz <62845504+Matheus-Schmitz@users.noreply.github.com>
2021-10-03 19:35:43 -0700

    fix space and tab indentation

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ classifiers = [
 
 setup(
 	name='mapta',
-	version='0.0.12',
+	version='0.0.02',
 	description='Classify a text as belonging to LGBTQ+ commuty and drug user community.',
 	py_modules=["Mapta",  "Pytorch_NN"],
 	author = 'Matheus Schmitz',
